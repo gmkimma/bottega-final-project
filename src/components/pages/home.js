7c53ce9d33332f02fd307d3e83de761b99febddb
Greg Kimma <gmkimma@gmail.com>
2023-05-17 20:30:01 -0700

    added action icons

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,52 +1,72 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import NavBar from '../navigation/navigation-container'
 
-export default class Home extends Component {
-  render () {
-    return (
-      <>
-        <NavBar />
+//images for buttons
+import surrender from '../../assets/images/home/surrender.png'
+import split from '../../assets/images/home/split.png'
+import double from '../../assets/images/home/double.png'
+import stand from '../../assets/images/home/stand.png'
+import hit from '../../assets/images/home/hit.png'
 
-        <div className='game-table'>
-          <div className='dealer-cards'>
-            <div className='card'>1</div>
-            <div className='card'>2</div>
-            <div className='dealer-total'>Dealer Total</div>
-          </div>
+export default function Home () {
+  return (
+    <>
+      <NavBar />
 
-          <div className='player-cards'>
-            <div className='card'>1</div>
-            <div className='card'>2</div>
-            <div className='player-total'>Player Total</div>
-          </div>
+      <div className='game-table'>
+        <div className='dealer-cards'>
+          <div className='card'></div>
+          <div className='card'></div>
+          <div className='dealer-total'>Dealer Total</div>
+        </div>
 
-          <div className='action-wrapper'>
-            <div className='action'>Surrender</div>
-            <div className='action'>Split</div>
-            <div className='action'>Double</div>
-            <div className='action'>Stand</div>
-            <div className='action'>Hit</div>
-          </div>
-
-          <div className='odds-wrapper'>
-            Odds of next card
-            <ul>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>5</li>
-            </ul>
-          </div>
-
-          <div className='strategy-wrapper'>
-            <div className='strategy'>
-              <h3>You should probably do this...</h3>
-            </div>
+        <div className='strategy-wrapper'>
+          <div className='strategy'>
+            <h3>You should probably do this...</h3>
           </div>
         </div>
-      </>
-    )
-  }
+
+        <div className='player-cards'>
+          <div className='card'></div>
+          <div className='card'></div>
+          <div className='player-total'>Player Total</div>
+        </div>
+
+        <div className='action-wrapper'>
+          <div className='action'>
+            <img src={surrender} alt='surrender' />
+            Surrender
+          </div>
+          <div className='action'>
+            <img src={split} alt='split' />
+            Split
+          </div>
+          <div className='action'>
+            <img src={double} alt='double' />
+            Double
+          </div>
+          <div className='action'>
+            <img src={stand} alt='stand' />
+            Stand
+          </div>
+          <div className='action'>
+            <img src={hit} alt='hit' />
+            Hit
+          </div>
+        </div>
+
+        <div className='odds-wrapper'>
+          Odds of next card
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
 }
