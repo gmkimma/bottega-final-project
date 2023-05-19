550ce130cf4e8c3964c2b5c6a805695ec5f2d9c7
Greg Kimma <gmkimma@gmail.com>
2023-05-19 04:21:50 -0700

    Refactored show deck to functional component

--- src/components/deck/show-deck.js
+++ src/components/deck/show-deck.js
@@ -1,107 +1,93 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import Deck from './deck'
 
-export default class ShowDeck extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      deck: Deck()
-    }
-
-    this.handleClickCard = this.handleClickCard.bind(this)
-  }
+export default function ShowDeck (props) {
+  const deck = Deck()
 
   //Returns the card that is clicked in the card modal
-  handleClickCard = e => {
-    this.props.handleCardSelection(e.target.outerText)
+  function handleClickCard (e) {
+    props.handleCardSelection(e.target.outerText)
   }
 
-  render () {
-    //creates an array for the spade cards
-    const spadeCards = []
-    for (let i = 0; i < 13; i++) {
-      spadeCards.push(this.state.deck[i])
-    }
-
-    //creates an array for the heart cards
-    const heartCards = []
-    for (let i = 13; i < 26; i++) {
-      heartCards.push(this.state.deck[i])
-    }
-
-    //creates an array for the club cards
-    const clubCards = []
-    for (let i = 26; i < 39; i++) {
-      clubCards.push(this.state.deck[i])
-    }
-
-    //creates an array for the diamond cards
-    const diamondCards = []
-    for (let i = 39; i < 52; i++) {
-      diamondCards.push(this.state.deck[i])
-    }
-
-    return (
-      <>
-        {/* Created 4 divs to present the deck in 4 rows of 13 cards of each suit */}
-        <div className='spade-wrapper'>
-          {spadeCards.map((card, index) => {
-            return (
-              <div
-                key={index}
-                className='visible-card'
-                onClick={this.handleClickCard}
-              >
-                {card}
-              </div>
-            )
-          })}
-        </div>
-
-        <div className='heart-wrapper'>
-          {heartCards.map((card, index) => {
-            return (
-              <div
-                key={index + 13}
-                className='visible-card'
-                onClick={this.handleClickCard}
-              >
-                {card}
-              </div>
-            )
-          })}
-        </div>
-
-        <div className='club-wrapper'>
-          {clubCards.map((card, index) => {
-            return (
-              <div
-                key={index + 26}
-                className='visible-card'
-                onClick={this.handleClickCard}
-              >
-                {card}
-              </div>
-            )
-          })}
-        </div>
-
-        <div className='diamond-wrapper'>
-          {diamondCards.map((card, index) => {
-            return (
-              <div
-                key={index + 39}
-                className='visible-card'
-                onClick={this.handleClickCard}
-              >
-                {card}
-              </div>
-            )
-          })}
-        </div>
-      </>
-    )
+  //Creates an array for the spade cards
+  const spadeCards = []
+  for (let i = 0; i < 13; i++) {
+    spadeCards.push(deck[i])
   }
+
+  //Creates an array for the heart cards
+  const heartCards = []
+  for (let i = 13; i < 26; i++) {
+    heartCards.push(deck[i])
+  }
+
+  //Creates an array for the club cards
+  const clubCards = []
+  for (let i = 26; i < 39; i++) {
+    clubCards.push(deck[i])
+  }
+
+  //Creates an array for the diamond cards
+  const diamondCards = []
+  for (let i = 39; i < 52; i++) {
+    diamondCards.push(deck[i])
+  }
+
+  return (
+    <>
+      {/* Created 4 div wrappers to present the deck in 4 rows of 13 cards of each suit in the card modal*/}
+      <div className='spade-wrapper'>
+        {spadeCards.map((card, index) => {
+          return (
+            <div key={index} className='visible-card' onClick={handleClickCard}>
+              {card}
+            </div>
+          )
+        })}
+      </div>
+
+      <div className='heart-wrapper'>
+        {heartCards.map((card, index) => {
+          return (
+            <div
+              key={index + 13}
+              className='visible-card'
+              onClick={handleClickCard}
+            >
+              {card}
+            </div>
+          )
+        })}
+      </div>
+
+      <div className='club-wrapper'>
+        {clubCards.map((card, index) => {
+          return (
+            <div
+              key={index + 26}
+              className='visible-card'
+              onClick={handleClickCard}
+            >
+              {card}
+            </div>
+          )
+        })}
+      </div>
+
+      <div className='diamond-wrapper'>
+        {diamondCards.map((card, index) => {
+          return (
+            <div
+              key={index + 39}
+              className='visible-card'
+              onClick={handleClickCard}
+            >
+              {card}
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
 }
